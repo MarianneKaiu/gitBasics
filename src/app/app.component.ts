8cacdabb2de87c1a62b7350bd57cce42a81490cb
Marianne Lapierre <LApierre.marianne@gmail.com>
2023-07-10 15:16:11 +0200

    plop 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,7 @@ import { Component } from '@angular/core';
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet consequatur debitis dolore ducimus ea eveniet inventore ipsam, itaque libero, nobis, nostrum odit pariatur quis sapiente similique totam ullam veniam. Architecto assumenda atque distinctio eaque esse, et excepturi id molestias neque nulla numquam, officia, perferendis quis recusandae reprehenderit sit vero.</p>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium eaque, impedit! Aliquid architecto assumenda dolores ducimus et, excepturi harum impedit labore maiores nostrum obcaecati possimus, quo repellat rerum suscipit? Aliquid, architecto asperiores eaque et explicabo ipsa iure libero nam nisi non, optio pariatur, porro quos repellendus repudiandae tempore totam unde.</p>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci autem consectetur consequatur, corporis deserunt dolor dolorum, ducimus earum esse facere facilis fugit id in itaque iure iusto magnam nobis quam recusandae ullam. Corporis dolor expedita fugit magnam maxime non odio odit omnis pariatur totam. Aperiam error nobis non pariatur vero.</p>
-  <p>>> 1) Config user name
-    >>
-    >> git config --global user.name "Firstname Lastname"
-    >> 2) Config user email
-    >>
-    >> git config --global user.email your.email@allianz.com
-    >> 3) Config proxy
-    >>
-    >> git config --global http.proxy th000-surf.zone3.proxy.allianz:8080
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus aliquid asperiores aspernatur beatae doloribus dolorum ea, esse eum excepturi expedita fugiat illo labore minima mollitia nam natus nihil nostrum, odit possimus quam saepe sint, suscipit ullam velit! A dolor eligendi fuga iure laboriosam modi odit repellat temporibus tenetur vero.</p>`,
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
