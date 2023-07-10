858b78f3b418a01f6817b092e28344457e018d90
Marianne Lapierre <LApierre.marianne@gmail.com>
2023-07-10 15:17:12 +0200

    plop 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import { Component } from '@angular/core';
   template: `<h1>Hello world!</h1>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet consequatur debitis dolore ducimus ea eveniet inventore ipsam, itaque libero, nobis, nostrum odit pariatur quis sapiente similique totam ullam veniam. Architecto assumenda atque distinctio eaque esse, et excepturi id molestias neque nulla numquam, officia, perferendis quis recusandae reprehenderit sit vero.</p>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium eaque, impedit! Aliquid architecto assumenda dolores ducimus et, excepturi harum impedit labore maiores nostrum obcaecati possimus, quo repellat rerum suscipit? Aliquid, architecto asperiores eaque et explicabo ipsa iure libero nam nisi non, optio pariatur, porro quos repellendus repudiandae tempore totam unde.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci autem consectetur consequatur, corporis deserunt dolor dolorum, ducimus earum esse facere facilis fugit id in itaque iure iusto magnam nobis quam recusandae ullam. Corporis dolor expedita fugit magnam maxime non odio odit omnis pariatur totam. Aperiam error nobis non pariatur vero.</p>
   `,
   styleUrls: ['./app.component.css']
 })
