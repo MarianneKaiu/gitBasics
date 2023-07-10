263da2ba9682700e631f4b32781053b6724efec8
Marianne Lapierre <LApierre.marianne@gmail.com>
2023-07-10 15:36:39 +0200

    doc clean

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Hello world!</h1>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet consequatur debitis dolore ducimus ea eveniet inventore ipsam, itaque libero, nobis, nostrum odit pariatur quis sapiente similique totam ullam veniam. Architecto assumenda atque distinctio eaque esse, et excepturi id molestias neque nulla numquam, officia, perferendis quis recusandae reprehenderit sit vero.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium eaque, impedit! Aliquid architecto assumenda dolores ducimus et, excepturi harum impedit labore maiores nostrum obcaecati possimus, quo repellat rerum suscipit? Aliquid, architecto asperiores eaque et explicabo ipsa iure libero nam nisi non, optio pariatur, porro quos repellendus repudiandae tempore totam unde.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur debitis doloribus, eligendi illum nam optio ut. A atque consectetur debitis deleniti dicta distinctio dolore earum ex fugiat, fugit hic, natus nemo nihil nobis obcaecati quam quisquam recusandae reiciendis rerum sit sunt tempora velit voluptatem? Explicabo nam nesciunt repudiandae sed veritatis!</p>`,
+  template: `<h1>Hello world!</h1>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
